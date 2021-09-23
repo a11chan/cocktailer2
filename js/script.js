2ecdaa188acf2cafc5044db3703483ce83fe3d48
allchan <a11chan@hotmail.com>
2021-09-23 21:13:43 +0900

    add item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$(document).load(function() {
+$(document).ready(function () {
+    var tasteFilter = $(".submenu input");
+    var targetList = $(".carouselBox > .itemBox");
 
+    tasteFilter.click(function () {
+    var targetValue = [];
+    tasteFilter.filter(":checked").each(function () {
+        targetValue.push("." + $(this).val());
+    });
+    var targetClass = targetValue.join(", ");
+    targetList.hide();
+    $(targetClass).fadeIn();
+    console.log(targetClass);
+    });
 });
