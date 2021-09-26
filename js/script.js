816e5afa909d1615b458ca7fd652e2560ba61a65
allchan <a11chan@hotmail.com>
2021-09-27 05:34:47 +0900

    radio-checkbox

--- js/script.js
+++ js/script.js
@@ -15,9 +15,26 @@ $(document).ready(function () {
     targetList.hide();
     $(targetClass).fadeIn();
 
-    console.log(targetClass);
+    // console.log(targetClass);
     });
 
+
+
+    // 라디오-체크박스 적용 (inline-block처럼 서로의 특징 혼합)
+    $("#noseBtn").click(function(){
+        $('input[name="taste"]').not("#noseBtn").prop("checked",false);
+    });
+    $("#palateBtn").click(function(){
+        $('input[name="taste"]').not("#palateBtn").prop("checked",false);
+    });
+    $("#finishBtn").click(function(){
+        $('input[name="taste"]').not("#finishBtn").prop("checked",false);
+    });
+    $("#typeBtn").click(function(){
+        $('input[name="taste"]').not("#typeBtn").prop("checked",false);
+    });
+    // console.log($("#noseBtn").is(":checked"))
+    
 
     // 검색 결과 출력 토글
     $(".itemShow").hide();
